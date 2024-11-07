b30463176f9d357df6cdbba150f515d32a072371
Hjjj <wenonaliar@yahoo.com>
2024-11-07 15:40:12 -0800

    Initial implement of XLS reader.

--- Program.cs
+++ Program.cs
@@ -4,18 +4,58 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using ExcelDataReader;
+using System.IO;
 
 namespace CSVsToSQLServer
 {
     internal class Program
     {
+        static string DEST_TABLE_NAME = ConfigurationManager.AppSettings["DEST_TABLE_NAME"];
+        static string XLS_FOLDER = ConfigurationManager.AppSettings["XLS_FOLDER"];
+
         static void Main(string[] args)
         {
             //get a count of the rows in a sql db table
-            int count = GetRowCount("SELECT COUNT(*) FROM dbo.Connect_Ecard_Test");
+            int count = GetRowCount($"SELECT COUNT(*) FROM {DEST_TABLE_NAME}");
 
             //print the count to the console
             Console.WriteLine("The row count is: " + count);
+
+            //get count of files in the xls folder
+            string[] files = System.IO.Directory.GetFiles(XLS_FOLDER, "*.xlsx", System.IO.SearchOption.TopDirectoryOnly);
+            Console.WriteLine("The file count is: " + files.Length);
+
+            //open up an xlsx file and loop through the rows
+            string file = files[0];
+
+
+            using (var stream = File.Open(file, FileMode.Open, FileAccess.Read))
+            {
+                // Auto-detect format, supports:
+                //  - Binary Excel files (2.0-2003 format; *.xls)
+                //  - OpenXml Excel files (2007 format; *.xlsx, *.xlsb)
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    // Choose one of either 1 or 2:
+
+                    // 1. Use the reader methods
+                    do
+                    {
+                        while (reader.Read())
+                        {
+                            var mystring = reader.GetString(0);
+                        }
+
+                    } while (reader.NextResult());
+
+                    // 2. Use the AsDataSet extension method
+                    //var result = reader.AsDataSet();
+                    // The result of each spreadsheet is in result.Tables
+                }
+            }
+
 
             //readkey to keep the console open  
             Console.ReadKey();
@@ -24,8 +64,7 @@ namespace CSVsToSQLServer
         static int GetRowCount(string query)
         {
             int count = 0;
-            string connectionString = "Server=LDCAMRDEV1\\SQL2016;Database=Connect_Dev;User Id=webapi;Password=p@ssw0rd;";
-
+            string connectionString = ConfigurationManager.AppSettings["ConnectionString"];
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
