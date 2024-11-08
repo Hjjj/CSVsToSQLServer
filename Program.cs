fadc3c0c19ae8994e47b755f5d1ea290dbd4b60d
Hjjj <wenonaliar@yahoo.com>
2024-11-07 22:57:53 -0800

    added xlst processing. added sql servier inserting

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Configuration;
 using ExcelDataReader;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace CSVsToSQLServer
 {
@@ -17,6 +18,8 @@ namespace CSVsToSQLServer
 
         static void Main(string[] args)
         {
+            //TODO set up logging
+
             //get a count of the rows in a sql db table
             int count = GetRowCount($"SELECT COUNT(*) FROM {DEST_TABLE_NAME}");
 
@@ -30,6 +33,10 @@ namespace CSVsToSQLServer
             //open up an xlsx file and loop through the rows
             string file = files[0];
 
+            //TODO make is pull multiple xlsx files from the folder
+            //TODO make it resilent to repetitive restarts
+            //so a sqlite log of which xlsx files it has already processed
+            //and a sqlite log of which rows it has already processed
 
             using (var stream = File.Open(file, FileMode.Open, FileAccess.Read))
             {
@@ -38,27 +45,134 @@ namespace CSVsToSQLServer
                 //  - OpenXml Excel files (2007 format; *.xlsx, *.xlsb)
                 using (var reader = ExcelReaderFactory.CreateReader(stream))
                 {
-                    // Choose one of either 1 or 2:
-
-                    // 1. Use the reader methods
                     do
                     {
                         while (reader.Read())
                         {
-                            var mystring = reader.GetString(0);
+                            //TODO put all of this into a try catch block
+
+                            var certTitle =  reader.GetString((int)Col.CertTitle);
+                            certTitle = certTitle?.Length > 200 ? certTitle.Substring(0, 200) : certTitle;
+
+                            var fullName = reader.GetString((int)Col.FullName);
+                            fullName = fullName?.Length > 100 ? fullName.Substring(0, 100) : fullName;
+
+                            if (certTitle=="CertTitle" && fullName=="FullName")
+                            {
+                                //we are in a title row, so skip it.
+                                continue;
+                            }
+
+                            DateTime issueDate = reader.GetDateTime((int)Col.IssueDate);
+                            string renewByDate = FormatToRenewByDate(reader.GetString((int)Col.RenewByDate));
+                            var eCardCode = reader.GetString((int)Col.ECardCode);
+                            var fileName = reader.GetString((int)Col.FileName);
+                            if(!IsValidFileName(fileName))
+                            {
+                                //log the error
+                                continue;
+                            }
+                            fileName = Path.GetFileNameWithoutExtension(fileName);
+                            int employeeId = extractEmployeeId(fileName);
+                            int employeeCertificatesId = extractEmployeeCertificatesId(fileName);
+                            var OldCertName = extractOldCertName(fileName);
+
+                            //save the data to the sql server
+                            string connectionString = ConfigurationManager.AppSettings["ConnectionString"];
+                            using (SqlConnection connection = new SqlConnection(connectionString))
+                            {
+                                connection.Open();
+                                using (SqlCommand command = new SqlCommand(
+                                    $@"
+IF NOT EXISTS (SELECT 1 FROM {DEST_TABLE_NAME} WHERE EmployeeID = @EmployeeId AND DispositionID = @EmployeeCertificatesId AND OldCertName = @OldCertName)
+    BEGIN
+        INSERT INTO {DEST_TABLE_NAME} (CertTitle, FullName, IssueDate, RenewByDate, EcardCode, EmployeeID, DispositionID, OldCertName)
+        VALUES (@CertTitle, @FullName, @IssueDate, @RenewByDate, @ECardCode, @EmployeeId, @EmployeeCertificatesId, @OldCertName)
+    END", connection))
+                                {
+                                    command.Parameters.AddWithValue("@CertTitle", certTitle);
+                                    command.Parameters.AddWithValue("@FullName", fullName);
+                                    command.Parameters.AddWithValue("@IssueDate", issueDate);
+                                    command.Parameters.AddWithValue("@RenewByDate", renewByDate);
+                                    command.Parameters.AddWithValue("@ECardCode", eCardCode);
+                                    command.Parameters.AddWithValue("@EmployeeId", employeeId);
+                                    command.Parameters.AddWithValue("@EmployeeCertificatesId", employeeCertificatesId);
+                                    command.Parameters.AddWithValue("@OldCertName", OldCertName);
+
+                                    command.ExecuteNonQuery();
+                                }
+
+                            }
                         }
 
                     } while (reader.NextResult());
-
-                    // 2. Use the AsDataSet extension method
-                    //var result = reader.AsDataSet();
-                    // The result of each spreadsheet is in result.Tables
                 }
             }
 
-
             //readkey to keep the console open  
             Console.ReadKey();
+        }
+
+        /// <summary>
+        /// the file name format is "CertificateName_EmployeeCertificatesId_EmployeeId"
+        /// this extracts the CertificateName
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        private static object extractOldCertName(string fileName)
+        {
+            // Assuming the file name format is "CertificateName_EmployeeCertificatesId_EmployeeId"
+            var parts = fileName.Split('_');
+
+            if (parts.Length == 3)
+            {
+                return parts[0];
+            }
+            else
+            {
+                throw new FormatException("Invalid file name format");
+            }
+        }
+
+        /// <summary>
+        /// the file name format is "CertificateName_EmployeeCertificatesId_EmployeeId"
+        /// this function extracts the EmployeeCertificatesId
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        private static int extractEmployeeCertificatesId(string fileName)
+        {
+            // Assuming the file name format is "CertificateName_EmployeeCertificatesId_EmployeeId"
+            var parts = fileName.Split('_');
+
+            if (parts.Length == 3 && int.TryParse(parts[1], out int employeeCertificatesId))
+            {
+                return employeeCertificatesId;
+            }
+            else
+            {
+                throw new FormatException("Invalid file name format");
+            }
+        }
+
+        /// <summary>
+        /// takes a random string and formats it to mm/yyyy
+        /// </summary>
+        /// <param name="timeString"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        private static string FormatToRenewByDate(string timeString)
+        {
+            if (DateTime.TryParse(timeString, out DateTime date))
+            {
+                return date.ToString("MM/yyyy");
+            }
+            else
+            {
+                throw new FormatException($"Invalid date format. String was {timeString}");
+            }
         }
 
         static int GetRowCount(string query)
@@ -74,6 +188,58 @@ namespace CSVsToSQLServer
             }
 
             return count;
+        }
+
+        private static int extractEmployeeId(string fileName)
+        {
+            // Assuming the file name format is "CertificateName_EmployeeCertificatesId_EmployeeId"
+            var parts = fileName.Split('_');
+
+            if (parts.Length == 3 && int.TryParse(parts[2], out int employeeId))
+            {
+                return employeeId;
+            }
+            else
+            {
+                throw new FormatException("Invalid file name format");
+            }
+        }
+
+        /// <summary>
+        /// validates the chars in a filename
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        private static bool IsValidFileName(string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(fileName) || fileName == String.Empty)
+            {
+                return false;
+            }
+
+            //convert this full filepath to just the filename
+            fileName = Path.GetFileNameWithoutExtension(fileName);
+
+            char[] invalidChars = Path.GetInvalidFileNameChars();
+            foreach (char c in fileName)
+            {
+                if (invalidChars.Contains(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        enum Col
+        {
+            CertTitle = 0,
+            FullName = 1,
+            IssueDate = 2,
+            RenewByDate = 3,
+            ECardCode = 4,
+            FileName = 5
         }
     }
 }
